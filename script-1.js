f414a5a4287fcb89ae533fd2503d2a94582f088a
UltimatTomat <117816737+UltimatTomat@users.noreply.github.com>
2023-09-30 15:30:18 +0200

    Add files via upload

---
+++ script-1.js
@@ -0,0 +1,7 @@
+function hover() {
+    document.getElementById("home-image").src = "assets/home-2.png"
+  }
+  
+  function unhover() {
+    document.getElementById("home-image").src = "assets/home-1.png"
+  }
